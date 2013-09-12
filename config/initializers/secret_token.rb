5d769094eaf3fafc10c54ab5efc04360ec6ebd4a
LB426 <LB426@tih.kuban.ru>
2013-09-12 14:11:14 +0400

    создан проект stranasovetov

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Stranasovetov::Application.config.secret_key_base = 'b40fe41286eb09f7d12fe35a5fdaf9f63ca7de75a187a5a0ccd57aec07ee3e7b3b715c55a41c45f7acc2fb18b23e82f8d3806947e2d8185befa5f97c6d90e4bf'
