695dc4d8c6fda61b932de80a00b22296c97549b4
LB426 <LB426@tih.kuban.ru>
2013-09-19 17:02:56 +0400

    добавил аватаров к юзерам

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,7 +18,7 @@ class User < ActiveRecord::Base
                     :styles => { :medium => "300x300>", :thumb => "100x100>" },
                     :path => ":rails_root/public/images/avatars/:id/:style/:basename.:extension",
                     :url  => "/images/avatars/:id/:style/:basename.:extension",
-                    :default_url => "/images/avatar-missing.png"
+                    :default_url => "/images/avatar1853-1.jpg"
 
   def self.authenticate(login, password)
     user = find_by_login(login)
